da4a596c8be362d17037ceac7779748db90eaeb2
Ondrej Mejzlik <omejzlik@redhat.com>
2024-09-10 00:12:48 +0200

    add basic blinking no IDE code

---
+++ No-IDE-Blink/Attiny13a/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Compiling..."
+avr-gcc -Wall -g -Os -mmcu=attiny13a -o blink.bin blink.c
+if [ $? -eq 0 ]; then
+    avr-objcopy -j .text -j .data -O ihex blink.bin blink.hex
+    echo "Done"
+    echo "Flashing:"
+    avrdude -c usbasp -p attiny13 -U flash:w:blink.hex
+else
+    echo "error"
+fi
