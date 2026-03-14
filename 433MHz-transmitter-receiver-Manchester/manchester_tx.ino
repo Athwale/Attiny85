3308bab11bbd752463082be5e1356aba1998ed31
Ondrej Mejzlik <omejzlik@redhat.com>
2026-03-14 11:25:51 +0100

    update rx

--- 433MHz-transmitter-receiver-Manchester/manchester_tx.ino
+++ 433MHz-transmitter-receiver-Manchester/manchester_tx.ino
@@ -23,25 +23,53 @@
   MAN_38400 7
 */
 
-// PHY pin 2 -> SW Pin 3
-// PHY pin 3 -> SW Pin 4
-// PHY pin 5 -> SW Pin 0
-// PHY pin 6 -> SW Pin 1
-// PHY pin 7 -> SW Pin 2
+// PHY pin 5 -> SW Pin 0 - radio transmitter
+// PHY pin 6 -> SW Pin 1 - radar sensor
+// PHY pin 7 -> SW Pin 2 - pir sensor
+// PHY pin 2 -> SW Pin 3 - self reset
+// PHY pin 3 -> SW Pin 4 - status LED 
 
 #define TX_PIN 0
-#define PIR_PIN 1
+#define RADAR_PIN 1
+#define PIR_PIN 2
+#define RESET_PIN 3
+#define LED_PIN 4
+#define BLINK_INTERVAL 300
+
 int16_t transmit_data = 32;
+
+int radar_state = 0;
+int pir_state = 0;
 
 void setup() {
   delay(500);
+  pinMode(RADAR_PIN, INPUT);
   pinMode(PIR_PIN, INPUT);
+
+  //todo does not work.
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, HIGH);
+  
+  pinMode(LED_PIN, OUTPUT);
+  // Sets it's pin as output on it's own.
   man.setupTransmit(TX_PIN, MAN_300);
+
+  // Blink status led on boot finish.
+  digitalWrite(LED_PIN, HIGH);
+  delay(BLINK_INTERVAL);
+  digitalWrite(LED_PIN, LOW);
+  delay(BLINK_INTERVAL);
 } 
 
 void loop() {
-  int pir_state = digitalRead(PIR_PIN);
+  radar_state = digitalRead(RADAR_PIN);
+  pir_state = digitalRead(PIR_PIN);
+
   if (pir_state == 1 ) {
+      man.transmit(transmit_data);
+  }
+  
+  if (radar_state == 1 ) {
       man.transmit(transmit_data);
   } 
   delay(1000);
